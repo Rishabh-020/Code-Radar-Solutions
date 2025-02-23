fcae9c4170761eab308858dbbaefccb967ee7428
RISHABH AGGARWAL <rishabh5824sjps@gmail.com>
2025-02-23 15:16:22 +0530

    Solution to Find the Pair with the Minimum Difference

--- Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
+++ Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 void absolute_diff(int arr[],int num){
-    for(int i=0;i<num-1;i++){
-        for(int j=0;j<num-i-1;j++){
-            if(arr[j]-arr[j+1]||arr[j+1]-arr[j]=1)
-        }
-    }
+    // for(int i=0;i<num-1;i++){
+    //     for(int j=0;j<num-i-1;j++){
+    //         if(arr[j]-arr[j+1]||arr[j+1]-arr[j]=1)
+    //     }
+    // }
 }
 int main(){
     int num;
