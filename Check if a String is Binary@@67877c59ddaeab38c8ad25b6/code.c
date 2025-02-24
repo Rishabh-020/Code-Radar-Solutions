1df44f43c162450b2eb95672d0acf1fda1f6ec46
RISHABH AGGARWAL <rishabh5824sjps@gmail.com>
2025-02-24 23:18:30 +0530

    Solution to Check if a String is Binary

--- Check if a String is Binary@@67877c59ddaeab38c8ad25b6/code.c
+++ Check if a String is Binary@@67877c59ddaeab38c8ad25b6/code.c
@@ -2,42 +2,22 @@
 #include<string.h>
 #include<ctype.h>
 int main(){
-    // int num,count=1,N=0,len=0;
-    // scanf("%d",&num);
-    // while(num>0){
-    //     N=num%10;
-    //     if(isalpha(N)){
-    //         printf("No");
-    //         return 0;
-    //     }
-    //     if(N==1 || N==0){
-    //         len++;
-    //     }else{
-    //         count=0;
-    //     }
-    //     num/=10;
-    // }
-    // if(count==0){
-    //     printf("No");
-    // }else{
-    //     printf("Yes");
-    // }
-    char str[1000];
-    int count=0;
-    fgets(str,1000,stdin);
-    if(str[strlen(str)-1]=='\n'){
-        str[strlen(str)-1]='\0';
-    }
-    for(int i=0;i<strlen(str)-1;i++){
-        if(isdigit(str[i])){
-            if(str[i]==1 || str[i]==0){
-                count=0;
-            }
-        }else{
-            count=1;
+    int num,count=1,N=0,len=0;
+    scanf("%d",&num);
+    while(num>0){
+        N=num%10;
+        if(isalpha(N)){
+            printf("No");
+            return 0;
         }
+        if(N==1 || N==0){
+            len++;
+        }else{
+            count=0;
+        }
+        num/=10;
     }
-    if(count==1){
+    if(count==0){
         printf("No");
     }else{
         printf("Yes");
