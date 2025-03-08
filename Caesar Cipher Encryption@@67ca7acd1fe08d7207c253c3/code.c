38ab93d275edb18c652c005f8dc37cb51a0a15f9
RISHABH AGGARWAL <rishabh5824sjps@gmail.com>
2025-03-08 12:23:25 +0530

    Solution to Caesar Cipher Encryption

---
+++ Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int caesarCipher(char message[],int shift,char encrypted[]){
+    for(int i=0;i!='\0';i++){
+        if(message[i]" "){
+            encrypted[i]=(message[i]+shift)%26;
+        }
+    }
+}
