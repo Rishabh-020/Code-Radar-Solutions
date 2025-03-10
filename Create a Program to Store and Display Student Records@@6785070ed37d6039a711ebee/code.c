d6dcfda69f3717bdddca352092beb8c588098a5d
RISHABH AGGARWAL <rishabh5824sjps@gmail.com>
2025-03-10 18:55:26 +0530

    Solution to Create a Program to Store and Display Student Records

--- Create a Program to Store and Display Student Records@@6785070ed37d6039a711ebee/code.c
+++ Create a Program to Store and Display Student Records@@6785070ed37d6039a711ebee/code.c
@@ -18,9 +18,9 @@ int main()
         scanf("%f",&student[i].grade);
     }
     for(int i=0;i<num;i++){
-        printf("Student Id:%d\n",student[i].id );
-        printf("Student Name:%s\n",student[i].name);
-        printf("Student Grade:%.2f\n",student[i].grade);
+        printf("Student Id: %d, ",student[i].id );
+        printf("Student Name: %s, ",student[i].name);
+        printf("Student Grade: %.2f, ",student[i].grade);
     }
     return 0;
 }
