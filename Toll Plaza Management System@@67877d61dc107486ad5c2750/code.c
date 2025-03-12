68082ec81d2fc5300ab5abb45db7a0a1949d35fb
RISHABH AGGARWAL <rishabh5824sjps@gmail.com>
2025-03-12 11:13:43 +0530

    Solution to Toll Plaza Management System

--- Toll Plaza Management System@@67877d61dc107486ad5c2750/code.c
+++ Toll Plaza Management System@@67877d61dc107486ad5c2750/code.c
@@ -12,7 +12,7 @@ int main(){
         scanf("%s %s %d",&t[i].plate,&t[i].name,&t[i].price);
         if(t[i].name=="Car"){
             c1+=t[i].price;
-        }else if(t[i].name=='Truck'){
+        }else if(t[i].name=="Truck"){
             c2+=t[i].price;
         }else{
             c3+=t[i].price;
