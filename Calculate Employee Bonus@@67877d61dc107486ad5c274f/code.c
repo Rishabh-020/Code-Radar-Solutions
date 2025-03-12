dff02374892e360671925ed3355ad095818714f4
RISHABH AGGARWAL <rishabh5824sjps@gmail.com>
2025-03-12 11:04:03 +0530

    Solution to Calculate Employee Bonus

--- Calculate Employee Bonus@@67877d61dc107486ad5c274f/code.c
+++ Calculate Employee Bonus@@67877d61dc107486ad5c274f/code.c
@@ -3,10 +3,10 @@ struct Employee{
     int id;
     char name[100];
     int salary;
-    float bonus;
 };
 int main(){
     int num;
+    float bonus[num];
     scanf("%d",&num);
     struct Employee employee[num];
     for(int i=0;i<num;i++){
@@ -14,11 +14,11 @@ int main(){
     }
     for(int i=0;i<num;i++){
         if(employee[i].salary<50000){
-            employee[i].bonus=0.1*employee[i].salary;
+            bonus[i]=0.1*employee[i].salary;
         }else{
-            employee[i].bonus=(float)(5*employee[i].salary)/100;
+            bonus[i]=(float)(5*employee[i].salary)/100;
         }
         printf("Employee ID: %d, Name: %s, Bonus: %.2f\n"
-        ,employee[i].id,employee[i].name,employee[i].bonus);
+        ,employee[i].id,employee[i].name,bonus[i]);
     }
 }
