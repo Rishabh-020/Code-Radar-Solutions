e883b06b418fcda94b091c7b097cacb62dfb5a2e
RISHABH AGGARWAL <rishabh5824sjps@gmail.com>
2025-03-12 11:14:09 +0530

    Solution to Toll Plaza Management System

--- Toll Plaza Management System@@67877d61dc107486ad5c2750/code.c
+++ Toll Plaza Management System@@67877d61dc107486ad5c2750/code.c
@@ -5,7 +5,8 @@ struct toll{
     int price;
 };
 int main(){
-    int num,c1=0,c2=0,c3=0;
+    int num;
+    float c1=0.00,c2=0.00,c3=0.00;
     scanf("%d",&num);
     struct toll t[num];
     for(int i=0;i<num;i++){
@@ -18,5 +19,5 @@ int main(){
             c3+=t[i].price;
         }
     }
-    for(int i=0;i<num;i++)printf("Cars: %.2f, Trucks: %.2f, Bikes: %.2f",c1,c2,c3);
+    for(int i=0;i<num;i++)printf("Cars: %.2f, Trucks: %.2f, Bikes: %.2f\n",c1,c2,c3);
 }
