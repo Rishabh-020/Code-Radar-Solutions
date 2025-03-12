f29c0ce238f5d85c4e8bb8cafeae51805af4c282
RISHABH AGGARWAL <rishabh5824sjps@gmail.com>
2025-03-12 11:21:54 +0530

    Solution to Streaming Service Subscription Management

---
+++ Streaming Service Subscription Management@@67877d61dc107486ad5c2752/code.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string.h>
+struct toll{
+    char plate[100];
+    char name[100];
+    int price;
+};
+int main(){
+    int num,count1=0,count2=0,count3=0;
+    float c1=0.00,c2=0.00,c3=0.00;
+    scanf("%d",&num);
+    struct toll t[num];
+    for(int i=0;i<num;i++){
+        scanf("%s %s %d",&t[i].plate,&t[i].name,&t[i].price);
+        if(strcmp(t[i].name,"Basic")==0){
+            c1+=t[i].price;
+            count1++;
+        }else if(strcmp(t[i].name,"Standaed")==0){
+            c2+=t[i].price;
+            count2++;
+        }else{
+            c3+=t[i].price;
+            count3++;
+        }
+    }
+    printf("Basic: %d Users, Revenue: %.2f, Standaed: %d Users, Revenue: %.2f, Premium: %d Users, Revenue: %.2f\n",c1,c2,c3);
+}
