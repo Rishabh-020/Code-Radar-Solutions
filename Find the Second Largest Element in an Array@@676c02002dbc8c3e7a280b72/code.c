ac6b4387145d145a094c49100c2ef929774d471c
RISHABH AGGARWAL <rishabh5824sjps@gmail.com>
2025-03-13 11:12:00 +0530

    Solution to Find the Second Largest Element in an Array

--- Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
+++ Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
@@ -1,27 +1,54 @@
-#include <stdio.h>
+// #include <stdio.h>
+// #include<limits.h>
+// int main() {
+//     int num;
+//     scanf("%d",&num);
+//     int arr[num];
+//     for(int i=0;i<num;i++){
+//         scanf("%d",&arr[i]);
+//     }
+//     int largest=INT_MIN,sec_largest=INT_MIN;
+//     for(int i=0;i<num;i++){
+//         if(arr[i]>largest){
+//             sec_largest=largest;
+//             largest=arr[i];
+//         }else if(arr[i]> sec_largest && arr[i]!=largest){
+//             sec_largest=arr[i];
+//         }
+//     }
+//     if(sec_largest==INT_MIN){
+//         printf("%d",-1);
+//     }else{
+//         printf("%d",sec_largest);
+//     }
+//     return 0;
+// }
+#include<stdio.h>
 #include<limits.h>
-
-
-int main() {
+int main(){
     int num;
     scanf("%d",&num);
     int arr[num];
-    for(int i=0;i<num;i++){
-        scanf("%d",&arr[i]);
-    }
-    int largest=INT_MIN,sec_largest=INT_MIN;
+    int largest=INT_MIN,sec_largest=0;
+    for(int i=0;i<num;i++)scanf("%d",&arr[i]);
     for(int i=0;i<num;i++){
         if(arr[i]>largest){
             sec_largest=largest;
             largest=arr[i];
-        }else if(arr[i]> sec_largest && arr[i]!=largest){
-            sec_largest=arr[i];
         }
     }
-    if(sec_largest==INT_MIN){
-        printf("%d",-1);
-    }else{
-        printf("%d",sec_largest);
-    }
-    return 0;
+    printf("%d",sec_largest);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
