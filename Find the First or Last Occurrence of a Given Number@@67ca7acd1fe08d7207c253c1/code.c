71f03392bd14e131baf48457d41b0a066773b357
RISHABH AGGARWAL <rishabh5824sjps@gmail.com>
2025-03-14 18:09:40 +0530

    Solution to Find the First or Last Occurrence of a Given Number

--- Find the First or Last Occurrence of a Given Number@@67ca7acd1fe08d7207c253c1/code.c
+++ Find the First or Last Occurrence of a Given Number@@67ca7acd1fe08d7207c253c1/code.c
@@ -1,7 +1,9 @@
 int findOccurrence(int arr,int num,int target,char mode){
     if(mode=='L'){
         for(int i=0;i<num;i++){
-        if(arr[i]==target) find=i;
+        if(arr[i]==target){
+            find=i;
+        }
     }
     }if(mode=='F'){
         for(int i=0;i<num;i++){
