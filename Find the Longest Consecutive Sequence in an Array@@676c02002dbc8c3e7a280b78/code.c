6398370af0890039f7b2796a50e5e8efbcc0c03d
RISHABH AGGARWAL <rishabh5824sjps@gmail.com>
2025-03-15 21:45:20 +0530

    Solution to Find the Longest Consecutive Sequence in an Array

--- Find the Longest Consecutive Sequence in an Array@@676c02002dbc8c3e7a280b78/code.c
+++ Find the Longest Consecutive Sequence in an Array@@676c02002dbc8c3e7a280b78/code.c
@@ -1,65 +1,26 @@
-// #include<stdio.h>
-// #include<string.h>
-// int longest(int arr[],int num){
-//     int hash[1000]={0},max_len=0,count=0;
-//     for(int i=0;i<num;i++)hash[arr[i]]=1;
-//     for(int i=0;i<num;i++){
-//         if(hash[arr[i]-1]==0){
-//             int n=arr[i];
-//             count=0;
-//             while(hash[n]){
-//             count++;
-//             n++;
-//             }
-//         }if(max_len < count)max_len=count;
-//     }printf("%d 1",max_len);
-// }
-// int main(){
-//     int num;
-//     scanf("%d",&num);
-//     int arr[num];
-//     for(int i=0;i<num;i++)scanf("%d",&arr[i]);
-//     longest(arr,num);
-    
-//     return 0;
-// }
-
-#include <stdio.h>
-#include <string.h>
-
-int longest(int arr[], int num) {
-    int hash[100000] = {0}, max_len = 0;
-
-    // Mark elements in hash table
-    for (int i = 0; i < num; i++)
-        hash[arr[i]] = 1;
-
-    // Find longest consecutive sequence
-    for (int i = 0; i < num; i++) {
-        if (hash[arr[i] - 1] == 0) { // Starting point of a sequence
-            int n = arr[i], count = 0;
-            
-            while (hash[n]) { // Count length of the sequence
-                count++;
-                n++;
+#include<stdio.h>
+#include<string.h>
+int longest(int arr[],int num){
+    int hash[1000]={0},max_len=0,count=0;
+    for(int i=0;i<num;i++)hash[arr[i]]=1;
+    for(int i=0;i<num;i++){
+        if(hash[arr[i]-1]==0){
+            int n=arr[i];
+            count=0;
+            while(hash[n]){
+            count++;
+            n++;
             }
-            if (max_len < count) max_len = count;
+            if(max_len < count)max_len=count;
         }
-    }
-
-    printf("%d\n", max_len);
-    return max_len+1;
+    }printf("%d 1",max_len);
 }
-
-int main() {
+int main(){
     int num;
-    scanf("%d", &num);
+    scanf("%d",&num);
     int arr[num];
-
-    for (int i = 0; i < num; i++)
-        scanf("%d", &arr[i]);
-
-    longest(arr, num);
-
+    for(int i=0;i<num;i++)scanf("%d",&arr[i]);
+    longest(arr,num);
+    
     return 0;
 }
