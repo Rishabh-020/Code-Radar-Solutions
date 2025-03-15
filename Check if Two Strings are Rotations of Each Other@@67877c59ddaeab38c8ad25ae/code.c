f1b009d5c1d90ef28a18e7483dcf65b37625a000
RISHABH AGGARWAL <rishabh5824sjps@gmail.com>
2025-03-15 17:54:41 +0530

    Solution to Check if Two Strings are Rotations of Each Other

--- Check if Two Strings are Rotations of Each Other@@67877c59ddaeab38c8ad25ae/code.c
+++ Check if Two Strings are Rotations of Each Other@@67877c59ddaeab38c8ad25ae/code.c
@@ -1,34 +1,29 @@
-#include <stdio.h>
-#include <string.h>
-
-// Function to check if str2 is a rotation of str1
-int isRotation(char str1[], char str2[]) {
-    int len1 = strlen(str1), len2 = strlen(str2);
-
-    if (len1 != len2) return 0;  // Lengths must be the same
-
-    // Create a new string by concatenating str1 with itself
-    char temp[2000];  
-    strcpy(temp, str1);
-    strcat(temp, str1);
-
-    // Check if str2 is a substring of temp
-    return strstr(temp, str2) != NULL;
-}
-
-int main() {
-    char str1[1000], str2[1000];
-    
-    fgets(str1, sizeof(str1), stdin);
-    if (str1[strlen(str1) - 1] == '\n') str1[strlen(str1) - 1] = '\0';
-
-    fgets(str2, sizeof(str2), stdin);
-    if (str2[strlen(str2) - 1] == '\n') str2[strlen(str2) - 1] = '\0';
-
-    if (isRotation(str1, str2))
-        printf("Yesd\n");
-    else
-        printf("No\n");
-
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str1[1000],str2[1000];
+    fgets(str1,1000,stdin);
+    int len1=strlen(str1),count=0;
+    if(str1[len1-1]=='\n'){
+        str1[len1-1]='\0';
+    }
+    fgets(str2,1000,stdin);
+    int len2=strlen(str2);
+    if(str2[len2-1]=='\n'){
+        str2[len2-1]='\0';
+    }
+    if(len1!=len2){
+        printf("No");return 0;
+    }
+    for(int i=0;i<len1;i++){
+        if((i>=0 && str1[i]==str2[i+1]) ||
+           (str1[i]==str2[i-1] && i>0) ||
+           (str1[i]==str2[i] && i<len2)){
+            count++;
+           }
+    }
+    if(count==len1){
+        printf("Yes");
+    }else printf("No");
     return 0;
 }
