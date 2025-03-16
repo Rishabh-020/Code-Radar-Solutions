ba5d39ef23fcfcf1fa3b2c4272ee3215bafb076d
RISHABH AGGARWAL <rishabh5824sjps@gmail.com>
2025-03-16 08:51:07 +0530

    Solution to Find the Longest Consecutive Sequence in an Array

--- Find the Longest Consecutive Sequence in an Array@@676c02002dbc8c3e7a280b78/code.c
+++ Find the Longest Consecutive Sequence in an Array@@676c02002dbc8c3e7a280b78/code.c
@@ -2,23 +2,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #define MAX 100000
-int longest(int arr[],int n){
-    // int hash[MAX]={0};
-    // for(int i=0;i<n;i++)
-    // hash[arr[i]]=1;
-    // int max_len=0;
-    // for(int i=0;i<n;i++){
-    //     if(hash[arr[i]-1]==0){
-    //         int num=arr[i];
-    //         int count=0;
-    //         while(hash[num]){
-    //         count++;
-    //         num++;
-    //         }
-    //         if(count>max_len)
-    //         max_len=count;
-    //     }
-    // }printf("%d",max_len);
+int longest(int arr[],int num){
     int hash[10000]={0},max_len=0;
     for(int i=0;i<num;i++)
     hash[arr[i]]=1;
