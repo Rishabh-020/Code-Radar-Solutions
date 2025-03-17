94dfc1931bee06aa20059be641e132370fcac9c0
RISHABH AGGARWAL <rishabh5824sjps@gmail.com>
2025-03-17 18:05:59 +0530

    Solution to Count Distinct Substrings

--- Count Distinct Substrings@@67877c59ddaeab38c8ad25b0/code.c
+++ Count Distinct Substrings@@67877c59ddaeab38c8ad25b0/code.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
 #include<string.h>
 int long_pal(char str[],int len){
-    int start=1,end=0;
+    int start=0,end=1;
     for(int i=0;i<len;i++){
         int left=i,right=i;
         while(left>=0 && right<len && str[left]==str[right]){
-            if(right-left+1>len){
+            if(right-left+1>end){
                 start=left;
                 end=right+1-left;
             }
         }left--;
         right++;
     }
-    for(int i=start;i<start-end;i++){
+    for(int i=start;i<start+end;i++){
         printf("%c",str[i]);
     }
 }
