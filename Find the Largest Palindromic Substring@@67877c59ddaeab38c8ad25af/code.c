842c1a74e6c8f4c8f722935f5ed2f9b430ba7b39
RISHABH AGGARWAL <rishabh5824sjps@gmail.com>
2025-03-17 18:20:20 +0530

    Solution to Find the Largest Palindromic Substring

--- Find the Largest Palindromic Substring@@67877c59ddaeab38c8ad25af/code.c
+++ Find the Largest Palindromic Substring@@67877c59ddaeab38c8ad25af/code.c
@@ -1,24 +1,29 @@
 #include<stdio.h>
 #include<string.h>
-int long_pal(char str[],int len){
-    if(len%2==0){
-        int left=len/2,right=len/2;
-    }else{
-        int left=(len+1)/2,right=(len+1)/2;
-        while((left+right-1)==len && str[left]==str[right]){
-            left--;
-            right++;
-        }
-        for(int i=left;i<=right;i++){
-        printf("%c",str[i]);
+char long_pal(char str[],int len){
+    int start=0,end=1;
+    for(int i=0;i<len;i++){
+        int left=i,right=i;
+        while(left>=0 && right<len && str[left]==str[right]){
+            if(right-left+1>end){
+                start=left;
+                end=right+1-left;
+            }
+        }left--;
+        right++;
     }
+    for(int i=start;i<start+end;i++){
+        printf("%c",str[i]);
     }
 }
 int main(){
     char str[1000];
     fgets(str,1000,stdin);
     int len=strlen(str);
-    if(str[len-1]=='\n')str[len-1]='\0';
+    if(str[len-1]=='\n'){
+        str[len-1]='\0';
+        len--;
+    }
     long_pal(str,len);
     return 0;
 }
