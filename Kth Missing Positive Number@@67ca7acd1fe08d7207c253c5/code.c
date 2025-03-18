ad5cb4eb9a7e670e1f0e11d2b8b00478493fe783
RISHABH AGGARWAL <rishabh5824sjps@gmail.com>
2025-03-18 11:04:06 +0530

    Solution to Kth Missing Positive Number

--- Kth Missing Positive Number@@67ca7acd1fe08d7207c253c5/code.c
+++ Kth Missing Positive Number@@67ca7acd1fe08d7207c253c5/code.c
@@ -1,6 +1,7 @@
 int findKthMissing(int arr[],int n,int k){
-    int hash[100];
-    for(int i=0;i<arr[n-1];i++){
-        hash[arr[i]]++;
-    }
+    // int hash[100];
+    // for(int i=0;i<arr[n-1];i++){
+    //     hash[arr[i]]++;
+    // }
+    return -1;
 }
