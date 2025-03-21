60c72b73642cbe6a3ca924301a46e31d346ab9a2
RISHABH AGGARWAL <rishabh5824sjps@gmail.com>
2025-03-21 11:09:12 +0530

    Solution to Find the Pair with the Minimum Difference

--- Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
+++ Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
@@ -1,27 +1,36 @@
 #include<stdio.h>
 #include <stdlib.h>
 #include<limits.h>
-int compare(const void *a,const void *b){
-    return (*(int *)a-*(int *)b);
-}
-int absolute_diff(int arr[],int num){
-    if(num<=1){
-        printf("%d",-1);
-        return 0;
-    }
-    int min=INT_MAX;
-    int first=-1,secound=-1;
-    qsort(arr,num,sizeof(int),compare);
-    for(int i=0;i<num;i++){
-        int diff=abs(arr[i]-arr[i+1]);
-        if(diff<min){
-            min=diff;
-            first=arr[i];
-            secound=arr[i+1];
+// int compare(const void *a,const void *b){
+//     return (*(int *)a-*(int *)b);
+// }
+// int absolute_diff(int arr[],int num){
+//     if(num<=1){
+//         printf("%d",-1);
+//         return 0;
+//     }
+//     int min=INT_MAX;
+//     int first=-1,secound=-1;
+//     qsort(arr,num,sizeof(int),compare);
+//     for(int i=0;i<num;i++){
+//         int diff=abs(arr[i]-arr[i+1]);
+//         if(diff<min){
+//             min=diff;
+//             first=arr[i];
+//             secound=arr[i+1];
+//         }
+//     }
+//     printf("%d %d",first,secound);
+//    return 0;
+// }
+
+int selection_sort(int arr[],int num){
+    for(int i=0;i<num-1;i++){
+        int find=i;
+        for(int j=0;j<num;j++){
+            
         }
     }
-    printf("%d %d",first,secound);
-   return 0;
 }
 int main(){
     int num;
@@ -30,6 +39,11 @@ int main(){
     for(int i=0;i<num;i++){
         scanf("%d ",&arr[i]);
     }
-    absolute_diff(arr,num);
+    selection_sort(arr,num);
+    // absolute_diff(arr,num);
     return 0;
 }
+
+
+
+
