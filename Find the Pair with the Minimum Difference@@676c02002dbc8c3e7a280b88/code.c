9e16627c5ee3d737701e764e983dbea10c5de7f0
RISHABH AGGARWAL <rishabh5824sjps@gmail.com>
2025-03-21 11:41:40 +0530

    Solution to Find the Pair with the Minimum Difference

--- Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
+++ Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
@@ -1,44 +1,27 @@
 #include<stdio.h>
 #include <stdlib.h>
 #include<limits.h>
-// int compare(const void *a,const void *b){
-//     return (*(int *)a-*(int *)b);
-// }
-// int absolute_diff(int arr[],int num){
-//     if(num<=1){
-//         printf("%d",-1);
-//         return 0;
-//     }
-//     int min=INT_MAX;
-//     int first=-1,secound=-1;
-//     qsort(arr,num,sizeof(int),compare);
-//     for(int i=0;i<num;i++){
-//         int diff=abs(arr[i]-arr[i+1]);
-//         if(diff<min){
-//             min=diff;
-//             first=arr[i];
-//             secound=arr[i+1];
-//         }
-//     }
-//     printf("%d %d",first,secound);
-//    return 0;
-// }
-
-int selection_sort(int arr[],int num){
-    for(int i=0;i<num-1;i++){
-        int find=i;
-        for(int j=i+1;j<num;j++){
-            if(arr[j]<find){
-                find=j;
-            }
-        }int temp=arr[i];
-        arr[i]=arr[find];
-        arr[find]=temp;
-    }
+int compare(const void *a,const void *b){
+    return (*(int *)a-*(int *)b);
 }
-int print_array(int arr[],int num){
-    int num1=arr[num-2],num2=arr[num-1];
-    printf("%d %d",num1,num2);
+int absolute_diff(int arr[],int num){
+    if(num<=1){
+        printf("%d",-1);
+        return 0;
+    }
+    int min=INT_MAX;
+    int first=-1,secound=-1;
+    qsort(arr,num,sizeof(int),compare);
+    for(int i=0;i<num;i++){
+        int diff=abs(arr[i]-arr[i+1]);
+        if(diff<min){
+            min=diff;
+            first=arr[i];
+            secound=arr[i+1];
+        }
+    }
+    printf("%d %d",first,secound);
+   return 0;
 }
 int main(){
     int num;
@@ -47,13 +30,10 @@ int main(){
         printf("%d",-1);
         return 0;
     }
-    
     int arr[num];
     for(int i=0;i<num;i++){
         scanf("%d ",&arr[i]);
     }
-    selection_sort(arr,num);
-    print_array(arr,num);
     // absolute_diff(arr,num);
     return 0;
 }
