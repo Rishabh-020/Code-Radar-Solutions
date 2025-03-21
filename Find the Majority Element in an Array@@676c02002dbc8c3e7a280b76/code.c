87e96150d2a6d72b8001d8c23de0a290fcd9fd57
RISHABH AGGARWAL <rishabh5824sjps@gmail.com>
2025-03-21 16:48:34 +0530

    Solution to Find the Majority Element in an Array

--- Find the Majority Element in an Array@@676c02002dbc8c3e7a280b76/code.c
+++ Find the Majority Element in an Array@@676c02002dbc8c3e7a280b76/code.c
@@ -1,29 +1,46 @@
+// #include<stdio.h>
+// int find_maj(int arr[],int num){
+//     int count=0,candidate=-1;
+//     for(int i=0;i<num;i++){
+//         if(count==0){
+//             candidate=arr[i];
+//             count=1;
+//         }else if(arr[i]==candidate)count++;
+//         else count--;
+//     }
+//     count=0;
+//     for(int i=0;i<num;i++){
+//         if(arr[i]==candidate){
+//             count++;
+//         }
+//     }
+//     if(count>num/2){
+//         return candidate;
+//     }
+//     return -1;
+// }
+// int main(){
+//     int num;
+//     scanf("%d",&num);
+//     int arr[num];
+//     for(int i=0;i<num;i++)scanf("%d",&arr[i]);
+//     printf("%d",find_maj(arr,num));
+//     return 0;
+// }
 #include<stdio.h>
-int find_maj(int arr[],int num){
-    int count=0,candidate=-1;
+int main(){
+    int num,max=-1;
+    scanf("%d",&num);
+    int arr[num],freq[1001]={0};
     for(int i=0;i<num;i++){
-        if(count==0){
-            candidate=arr[i];
-            count=1;
-        }else if(arr[i]==candidate)count++;
-        else count--;
+        scanf("%d",&arr[i]);
+        freq[arr[i]]++;
     }
-    count=0;
-    for(int i=0;i<num;i++){
-        if(arr[i]==candidate){
-            count++;
+    for(int i=0;i<1001;i++){
+        if(freq[i]>max && freq[i]>=(num/2)){
+            max=i;
         }
     }
-    if(count>num/2){
-        return candidate;
-    }
-    return -1;
-}
-int main(){
-    int num;
-    scanf("%d",&num);
-    int arr[num];
-    for(int i=0;i<num;i++)scanf("%d",&arr[i]);
-    printf("%d",find_maj(arr,num));
+    printf("%d",max);
     return 0;
 }
