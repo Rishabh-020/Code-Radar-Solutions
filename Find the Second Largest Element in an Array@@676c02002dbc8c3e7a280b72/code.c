4493bfaa61d45dbecd1b2475a95135e0d6fe5579
RISHABH AGGARWAL <rishabh5824sjps@gmail.com>
2025-03-28 13:35:13 +0530

    Solution to Find the Second Largest Element in an Array

--- Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
+++ Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
@@ -1,51 +1,42 @@
-// #include <stdio.h>
-// #include<limits.h>
-// int main() {
+#include<stdio.h>
+#include<limits.h>
+// int main(){
 //     int num;
 //     scanf("%d",&num);
 //     int arr[num];
-//     for(int i=0;i<num;i++){
-//         scanf("%d",&arr[i]);
+//     int largest=INT_MIN,sec_largest=0;
+//     if(num==1){
+//         printf("-1");
+//         return 0;
 //     }
-//     int largest=INT_MIN,sec_largest=INT_MIN;
+//     for(int i=0;i<num;i++)scanf("%d",&arr[i]);
 //     for(int i=0;i<num;i++){
 //         if(arr[i]>largest){
 //             sec_largest=largest;
 //             largest=arr[i];
-//         }else if(arr[i]> sec_largest && arr[i]!=largest){
-//             sec_largest=arr[i];
-//         }
-//     }
-//     if(sec_largest==INT_MIN){
-//         printf("%d",-1);
-//     }else{
-//         printf("%d",sec_largest);
-//     }
-//     return 0;
+//         }else if(sec_largest<arr[i] && arr[i]!=largest)sec_largest=arr[i];
+//     }if(sec_largest!=INT_MIN)
+//     printf("%d",sec_largest);
+//     else printf("-1");
 // }
-#include<stdio.h>
-#include<limits.h>
+
 int main(){
     int num;
     scanf("%d",&num);
     int arr[num];
-    int largest=INT_MIN,sec_largest=0;
-    if(num==1){
-        printf("-1");
-        return 0;
-    }
     for(int i=0;i<num;i++)scanf("%d",&arr[i]);
+    int first=INT_MIN,secound=INT_MIN;
     for(int i=0;i<num;i++){
-        if(arr[i]>largest){
-            sec_largest=largest;
-            largest=arr[i];
-        }else if(sec_largest<arr[i] && arr[i]!=largest)sec_largest=arr[i];
-    }if(sec_largest!=INT_MIN)
-    printf("%d",sec_largest);
-    else printf("-1");
+        for(int j=i+1;j<num;j++){
+            if(arr[i]>arr[j]){
+                int secound=first;
+                int first=arr[i];
+            }
+        }
+    }
+    printf("%d",secound);
+    return 0;
 }
-
-
 
 
 
