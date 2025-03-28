eba15a7fcb5ada42b7ac4703b2fcdea992879e56
RISHABH AGGARWAL <rishabh5824sjps@gmail.com>
2025-03-28 14:15:06 +0530

    Solution to Check for Palindrome Array

--- Check for Palindrome Array@@676c02002dbc8c3e7a280b7a/code.c
+++ Check for Palindrome Array@@676c02002dbc8c3e7a280b7a/code.c
@@ -1,30 +1,54 @@
 #include <stdio.h>
 
-int rev_array(int arr[],int n){
-    int start=0,end=n-1;
-    while(start<end){
-        if(arr[start]!=arr[end]){
-            return 0;
-        }
-        start++;
-        end--;
-    }
-}
-int main() {
+// int rev_array(int arr[],int n){
+//     int start=0,end=n-1;
+//     while(start<end){
+//         if(arr[start]!=arr[end]){
+//             return 0;
+//         }
+//         start++;
+//         end--;
+//     }
+// }
+// int main() {
+//     int num;
+//     scanf("%d",&num);
+//     int arr[num];
+//     for(int i=0;i<num;i++){
+//         scanf("%d",&arr[i]);
+//     }
+//     if(num==1){
+//         printf("YES");
+//         return 0;
+//     }
+//     if(rev_array(arr,num)){
+//         printf("YES");
+//     }else{
+//         printf("NO");
+//     }
+//     return 0;
+// }
+
+int main(){
     int num;
     scanf("%d",&num);
     int arr[num];
-    for(int i=0;i<num;i++){
-        scanf("%d",&arr[i]);
+    for(int i=0;i<num;i++)scanf("%d",&arr[i]);
+    for(int i=0;i<num/2;i++){
+        if(arr[i]!=arr[num-i]){
+            printf("NO");
+            return 0;
+        }
     }
-    if(num==1){
-        printf("YES");
-        return 0;
-    }
-    if(rev_array(arr,num)){
-        printf("YES");
-    }else{
-        printf("NO");
-    }
-    return 0;
+    printf("YES");
 }
+
+
+
+
+
+
+
+
+
+
