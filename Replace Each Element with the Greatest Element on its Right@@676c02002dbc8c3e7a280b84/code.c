f6317fa51766a6b9ab8f74c5f302b6fd3bd661c2
RISHABH AGGARWAL <rishabh5824sjps@gmail.com>
2025-03-28 17:01:31 +0530

    Solution to Replace Each Element with the Greatest Element on its Right

--- Replace Each Element with the Greatest Element on its Right@@676c02002dbc8c3e7a280b84/code.c
+++ Replace Each Element with the Greatest Element on its Right@@676c02002dbc8c3e7a280b84/code.c
@@ -1,39 +1,65 @@
 #include<stdio.h>
+// int main(){
+//     int num,max=-1;
+//     scanf("%d",&num);
+//     int arr[num];
+//     for(int i=0;i<num;i++){
+//         scanf("%d",&arr[i]);
+//     }
+//     // for(int i=0;i<num;i++){
+//     //     for(int j=i+1;j<num-1;j++){
+//     //         if(arr[j]>arr[j+1] && arr[j]>max){
+//     //             max=arr[j];
+//     //             arr[i]=arr[j];
+//     //         }else if(arr[j]<arr[j+1] && arr[j+1]>max){
+//     //             max=arr[j+1];
+//     //             arr[i]=arr[j+1];
+//     //         }
+//     //     }
+//     //     max=0;
+//     // }
+//     // for(int i=0;i<num-2;i++){
+//     //     printf("%d ",arr[i]);
+//     // }
+//     // if(num>1){
+//     //     printf("%d %d",arr[num-1],-1);
+//     // }
+//     for(int i=num-1;i>=0;i--){
+//         int temp=arr[i];
+//         arr[i]=max;
+//         if(temp>max){
+//             max=temp;
+//         }
+//     }
+//     for(int i=0;i<num;i++){
+//         printf("%d ",arr[i]);
+//     }
+//     return 0;
+// }
 int main(){
-    int num,max=-1;
+    int num;
     scanf("%d",&num);
     int arr[num];
+    for(int i=0;i<num;i++)scanf("%d",&arr[i]);
     for(int i=0;i<num;i++){
-        scanf("%d",&arr[i]);
-    }
-    // for(int i=0;i<num;i++){
-    //     for(int j=i+1;j<num-1;j++){
-    //         if(arr[j]>arr[j+1] && arr[j]>max){
-    //             max=arr[j];
-    //             arr[i]=arr[j];
-    //         }else if(arr[j]<arr[j+1] && arr[j+1]>max){
-    //             max=arr[j+1];
-    //             arr[i]=arr[j+1];
-    //         }
-    //     }
-    //     max=0;
-    // }
-    // for(int i=0;i<num-2;i++){
-    //     printf("%d ",arr[i]);
-    // }
-    // if(num>1){
-    //     printf("%d %d",arr[num-1],-1);
-    // }
-    for(int i=num-1;i>=0;i--){
-        int temp=arr[i];
-        arr[i]=max;
-        if(temp>max){
-            max=temp;
+        for(int j=i+1;j<num;j++){
+            if(arr[i]>arr[j]){
+                arr[i]=arr[j];
+            }
         }
     }
-    for(int i=0;i<num;i++){
-        printf("%d ",arr[i]);
-    }
+    for(int i=0;i<num;i++)printf("%d",arr[i]);
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
 
