2257363d251326a37cbde30d66da7b4321b05997
RISHABH AGGARWAL <rishabh5824sjps@gmail.com>
2025-03-29 23:30:23 +0530

    Solution to Find the Smallest Missing Positive Integer

--- Find the Smallest Missing Positive Integer@@676c02002dbc8c3e7a280b79/code.c
+++ Find the Smallest Missing Positive Integer@@676c02002dbc8c3e7a280b79/code.c
@@ -1,24 +1,50 @@
 #include<stdio.h>
-int main(){
-    int num,num1=0;
-    scanf("%d",&num);
-    int arr[num];
+// int main(){
+//     int num,num1=0;
+//     scanf("%d",&num);
+//     int arr[num];
+//     for(int i=0;i<num;i++){
+//         scanf("%d",&arr[i]);
+//     }
+//     for(int i=0;i<num;i++){
+//         while (arr[i] > 0 && arr[i] <= num && arr[arr[i] - 1] != arr[i]) {
+//             int temp = arr[i];
+//             arr[i] = arr[arr[i] - 1];
+//             arr[temp - 1] = temp;
+//         }
+//     }
+//     for(int i=0;i<num;i++){
+//         if(arr[i]!=i+1){
+//             printf("%d",i+1);
+//             return 0;
+//         }
+//     }
+//     printf("%d",num+1);
+//     return 0;
+// }
+void sorted(int arr[],int num){
     for(int i=0;i<num;i++){
-        scanf("%d",&arr[i]);
-    }
-    for(int i=0;i<num;i++){
-        while (arr[i] > 0 && arr[i] <= num && arr[arr[i] - 1] != arr[i]) {
-            int temp = arr[i];
-            arr[i] = arr[arr[i] - 1];
-            arr[temp - 1] = temp;
+        int select=i;
+        for(int j=0;j<num;j++){
+            if(arr[select]>arr[j]){
+                int temp=j;
+                j=select;
+                select=temp; 
+            }
         }
     }
+}
+int main(){
+    int num;
+    scanf("%d",&num);
+    int arr[num];
+    for(int i=0;i<num;i++)
+    scanf("%d",&arr[i]);
+    sorted(arr,num);
     for(int i=0;i<num;i++){
-        if(arr[i]!=i+1){
+        if(i!=i+1){
             printf("%d",i+1);
             return 0;
         }
     }
-    printf("%d",num+1);
-    return 0;
 }
