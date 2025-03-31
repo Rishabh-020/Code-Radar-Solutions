0dc20b2f7cd57bd8cfac67d010c811ffe02ab988
RISHABH AGGARWAL <rishabh5824sjps@gmail.com>
2025-03-31 17:46:08 +0530

    Solution to Selection Sort for Integers

--- Selection Sort for Integers@@67ca7acd1fe08d7207c253bc/code.c
+++ Selection Sort for Integers@@67ca7acd1fe08d7207c253bc/code.c
@@ -3,7 +3,7 @@ int selectionSort(int arr[],int num){
     for(int i=0;i<num-1;i++){
         int insert=i;
         for(int j=i+1;j<num;j++){
-            if(arr[j]>arr[insert]){
+            if(arr[j]<arr[insert]){
                 insert=j;
             }
         }
