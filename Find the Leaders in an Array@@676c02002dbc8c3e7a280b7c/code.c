cb86317b5682214bdf08de209e5233402d536360
RISHABH AGGARWAL <rishabh5824sjps@gmail.com>
2025-04-01 10:57:23 +0530

    Solution to Find the Leaders in an Array

--- Find the Leaders in an Array@@676c02002dbc8c3e7a280b7c/code.c
+++ Find the Leaders in an Array@@676c02002dbc8c3e7a280b7c/code.c
@@ -1,40 +1,40 @@
 #include<stdio.h>
-// int main(){
-//     int num,now;
-//     scanf("%d",&num);
-//     int arr[num];
-//     for(int i=0;i<num;i++)
-//     scanf("%d",&arr[i]);
-//     if(num==1){
-//         printf("%d",arr[0]);
-//         return 0;
-//     }
-//     for(int i=0;i<num-1;i++){
-//         now=1;
-//         for(int j=i+1;j<num;j++){
-//             if(arr[i]<arr[j]){
-//                 now=0;
-//             }
-//         }
-//         if(now==1){
-//             printf("%d ",arr[i]);
-//         }
-//     }printf("%d",arr[num-1]);
-//     return 0;
-// }
-
 int main(){
-    int num,count=0;
+    int num,now;
     scanf("%d",&num);
     int arr[num];
-    for(int i=0;i<num;i++)scanf("%d",&arr[i]);
-    for(int i=0;i<num;i++){
-        for(int j=i+1;j<num;j++){
-            if(arr[i]>=arr[j]){
-                count++;
-            }
-        }if(count==(num-i-1))printf("%d ",arr[i]);
-        count=0;
+    for(int i=0;i<num;i++)
+    scanf("%d",&arr[i]);
+    if(num==1){
+        printf("%d",arr[0]);
+        return 0;
     }
+    for(int i=0;i<num-1;i++){
+        now=1;
+        for(int j=i+1;j<num;j++){
+            if(arr[i]<arr[j]){
+                now=0;
+            }
+        }
+        if(now==1){
+            printf("%d ",arr[i]);
+        }
+    }printf("%d",arr[num-1]);
     return 0;
 }
+
+// int main(){
+//     int num,count=0;
+//     scanf("%d",&num);
+//     int arr[num];
+//     for(int i=0;i<num;i++)scanf("%d",&arr[i]);
+//     for(int i=0;i<num;i++){
+//         for(int j=i+1;j<num;j++){
+//             if(arr[i]>=arr[j]){
+//                 count++;
+//             }
+//         }if(count==(num-i-1))printf("%d ",arr[i]);
+//         count=0;
+//     }
+//     return 0;
+// }
