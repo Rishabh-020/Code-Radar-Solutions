6991a42d89858289d2bcf98ead3cb9cdb8e53916
RISHABH AGGARWAL <rishabh5824sjps@gmail.com>
2025-04-09 19:00:53 +0530

    Solution to Gaming Tournament – Most Played Game

---
+++ Gaming Tournament – Most Played Game@@67de00956ac3fec9afb90e18/code.c
@@ -0,0 +1,13 @@
+int mostPlayedGame(int arr,int num){
+    int freq[1001]={0},min=num;
+    for(int i=0;i<num;i++){
+        freq[arr[i]]++;
+    }
+    int max=0;
+    for(int i=0;i<num;i++){
+        if(freq[arr[i]]>max){
+            max=arr[i];
+        }
+    }
+    return max;
+}
