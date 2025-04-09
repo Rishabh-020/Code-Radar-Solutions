f314887e4475c70d76281a809a476f493b340d1c
RISHABH AGGARWAL <rishabh5824sjps@gmail.com>
2025-04-09 18:25:09 +0530

    Solution to Digit Sequence Mountain Peak Finder

--- Digit Sequence Mountain Peak Finder@@67de00956ac3fec9afb90e11/code.c
+++ Digit Sequence Mountain Peak Finder@@67de00956ac3fec9afb90e11/code.c
@@ -1,12 +1,27 @@
 int len(int num){
-    int len=0;
+    int len=0,find=num;
     while(num>0){
         // int N=num%10;
         // arr[i]=N;
         ++len;
         num/=10;
     }
-    printf("%d",len);
+    if(len%2==0){
+        return -1;
+    }
+    int arr[len],i=0;
+    while(find>0){
+        int N=find%10;
+        arr[i]=N;
+        i++;
+        find/=10;
+    }
+    for(int i=1;i<len-1;i++){
+        if(arr[i]>arr[i+1] && arr[i]>arr[i-1]){
+            int find=arr[i];
+        }
+    }
+    return find;
 }
 int mountainPeak(int num){
     len(num);
