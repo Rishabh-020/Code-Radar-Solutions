e2e17d3df66bf96c0812d877a7e08bb7242a91fa
RISHABH AGGARWAL <rishabh5824sjps@gmail.com>
2025-04-11 10:42:24 +0530

    Solution to Kth Missing Positive Number

--- Kth Missing Positive Number@@67ca7acd1fe08d7207c253c5/code.c
+++ Kth Missing Positive Number@@67ca7acd1fe08d7207c253c5/code.c
@@ -1,25 +1,40 @@
 int findKthMissing(int arr[],int num,int k){
-    int kth=0,value=1;
-    int new_arr[arr[num-1]];
-    for(int i=0;i<arr[num-1];i++){
-        new_arr[i]=0;
-    }
-    for(int i=0;i<arr[num-1];i++){
-        new_arr[i]=i+1;
-        // printf("%d ",new_arr[i]);
-    }
-    for(int i=0;i<arr[num-1];i++){
-        for(int j=i;j<num;j++){
-            if(new_arr[i]==arr[j]){
-                value=new_arr[i];
-                break;
-            }else{
-                kth++;
-                value=new_arr[i];
-                // break;
+    int missing=1,kth=0;
+    for(int i=0;;i++){
+        if(arr[i]==missing){
+            continue;
+        }
+        else{
+            missing++;
+            kth++;
+            if(kth==k){
+                return kth;
             }
         }
+        
     }
-    return value;
+    // return missing;
+    // int kth=0,value=1;
+    // int new_arr[arr[num-1]];
+    // for(int i=0;i<arr[num-1];i++){
+    //     new_arr[i]=0;
+    // }
+    // for(int i=0;i<arr[num-1];i++){
+    //     new_arr[i]=i+1;
+    //     // printf("%d ",new_arr[i]);
+    // }
+    // for(int i=0;i<arr[num-1];i++){
+    //     for(int j=i;j<num;j++){
+    //         if(new_arr[i]==arr[j]){
+    //             value=new_arr[i];
+    //             break;
+    //         }else{
+    //             kth++;
+    //             value=new_arr[i];
+    //             // break;
+    //         }
+    //     }
+    // }
+    // return value;
     // return arr[num-1]+1;
 }
